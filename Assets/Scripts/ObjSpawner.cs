61cb8a87b952a8a79e044ef75c9e59a9fdfb8ce1
gal0wsky <maciej.gawlowski.biznes@gmail.com>
2023-02-28 18:20:56 +0100

    Obj respi sie pod myszka

---
+++ Assets/Scripts/ObjSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z= 0;
+            Instantiate(obj, pos, Quaternion.identity);
+            Debug.Log(pos);
+        }
+    }
+}
