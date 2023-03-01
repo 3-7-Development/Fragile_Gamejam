72dd1282b48c785229faf5495abdb414ede6a7dc
Patryk Czekaj <patryk13290@gmail.com>
2023-03-01 22:31:44 +0100

    przechodzenie między widokami ui

--- Assets/Scripts/UI/LevelSelectView.cs
+++ Assets/Scripts/UI/LevelSelectView.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 
-public class LevelSelect : MonoBehaviour
+public class LevelSelectView : View
 {
-    private UIDocument document;
     [SerializeField] private VisualTreeAsset levelButtonTemplate;
     [SerializeField] private List<LevelObject> levels;
-    private void OnEnable()
-    {
-        document= GetComponent<UIDocument>();
+    private Button backButton;
 
-        foreach(LevelObject level in levels)
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        backButton = document.rootVisualElement.Q<Button>("BackButton");
+        backButton.clicked += () =>
+        {
+            ViewManager.Show<MainMenuView>();
+        };
+        foreach (LevelObject level in levels)
         {
             LevelButton newButton = new LevelButton(level, levelButtonTemplate);
             document.rootVisualElement.Q("LevelColumn").Add(newButton.button);
         }
-        
-        
     }
 }
+
