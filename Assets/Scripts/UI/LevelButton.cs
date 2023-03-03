460382de398228738f4ba0fc4426efb805174ae8
gal0wsky <maciej.gawlowski.biznes@gmail.com>
2023-03-03 16:38:42 +0100

    Mam dość

--- Assets/Scripts/UI/LevelButton.cs
+++ Assets/Scripts/UI/LevelButton.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+<<<<<<< HEAD
 using UnityEngine.SceneManagement;
 
+=======
+>>>>>>> 58197e4 (Napraw sie konflikcie)
 
 public class LevelButton
 {
     internal Button button;
     private LevelObject level;
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 58197e4 (Napraw sie konflikcie)
     public LevelButton(LevelObject level, VisualTreeAsset template)
     {
         this.level = level;
@@ -29,7 +35,11 @@ public class LevelButton
 
     private void OnClick(ClickEvent e)
     {
+<<<<<<< HEAD
         Debug.Log("Loading " + level.sceneName);
         SceneManager.LoadScene(level.sceneName);
+=======
+        Debug.Log("Loading" + level.sceneName);
+>>>>>>> 58197e4 (Napraw sie konflikcie)
     }
 }
